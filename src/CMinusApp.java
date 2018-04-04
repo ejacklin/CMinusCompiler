f448c781ead277a925ecaf75000c69684ce7d4a8
ejacklin <K1ssF1st!!>
2018-04-04 08:31:07 -0400

    Creates AST

--- src/CMinusApp.java
+++ src/CMinusApp.java
@@ -1,7 +1,30 @@
+import com.ejacklin.antlr.*;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import java.io.IOException;
+
 public class CMinusApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        CharStream inputStream = null;
+        try {
+             inputStream = CharStreams.fromFileName("/Users/jacklem1/IdeaProjects/CMinusCompiler/src/C-Input-1.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        CMinusCompilerLexer cMinusCompilerLexer = new CMinusCompilerLexer(inputStream);
+        CommonTokenStream commonTokenStream = new CommonTokenStream(cMinusCompilerLexer);
+        CMinusCompilerParser cMinusCompilerParser = new CMinusCompilerParser(commonTokenStream);
+        ParseTree parseTree = cMinusCompilerParser.program();
+
+        AST ast = new AST(parseTree);
+
+        System.out.println(ast);
+//
+//        CMinusCompilerParser.F.FileContext fileContext = cMinusCompilerParser.file();file
+//        ASTVisitor visitor = new ASTVisitor();
+//        visitor.visit(fileContext);
     }
 
 }
